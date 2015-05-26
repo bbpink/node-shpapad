dbc937b3d0bba849eaa9dbb8cd0f90292304b9b0
bbpink <ssbbpink@gmail.com>
2015-05-26 15:43:30 +0900

    assets response

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var ECT = require("ect");
+var fs = require("fs");
+
+//environments
 var app = express();
+var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect'});
+app.set('view engine', 'ect');
+app.engine("ect", ectRenderer.render);
+
+//assets
+app.get("/index.css", function(req, res) {
+  var f = fs.readFileSync(__dirname + "/assets/index.css");
+  res.writeHead(200, {"Content-Type":"text/css"});
+  res.end(f, "utf-8");
+});
+app.get("/index.js", function(req, res) {
+  var f = fs.readFileSync(__dirname + "/assets/index.js");
+  res.writeHead(200, {"Content-Type":"text/javascript"});
+  res.end(f, "utf-8");
+});
 
 app.get("/", function(req, res) {
-  console.log("kitayo");
-  res.send("hey");
+  res.render("index");
 });
 
 app.listen(3000);
